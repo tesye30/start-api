b56a51cd95366df86237ecce57968971ebc609b4
tesye30 <tesye30@hotmail.com>
2021-10-04 17:58:07 -0300

    adicionando tipos

--- dist/server.js
+++ dist/server.js
@@ -4,8 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var routes_1 = require("./routes");
 var app = (0, express_1.default)();
-app.get("/", function (request, response) {
-    return response.json({ message: "Hello Word" });
-});
+app.get("/", routes_1.createCourse);
 app.listen(8080);
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+import { createCourse } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello Word"})
-});
+app.get("/", createCourse);
 
 app.listen(8080);
