4091ea035b7e430e7684aa761b0b3c4b51b91db8
lucas couto <tesye30@hotmail.com>
2021-10-05 23:54:00 -0300

    debugando aplicaçao

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,14 @@ import express, { response } from 'express';
 
 const app = express();
 
+app.use(express.json());
 app.get("/", (request, response) =>{
     return response.json( {message: "Hello Word!"});
+})
+
+app.post("/courses", (request, response) => { 
+    const { name } = request.body;
+    return response.json({ name});
 })
 
 app.listen(8080, () => console.log("Server is running!"));
