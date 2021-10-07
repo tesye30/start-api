11e4badc34a5a2b3149dd36f2909c0acc800effc
lucas couto <tesye30@hotmail.com>
2021-10-06 23:33:42 -0300

    criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
 import express, { response } from 'express';
-
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
-app.get("/", (request, response) =>{
-    return response.json( {message: "Hello Word!"});
-})
 
-app.post("/courses", (request, response) => { 
-    const { name } = request.body;
-    return response.json({ name});
-})
-
+app.use(categoriesRoutes);
 app.listen(8080, () => console.log("Server is running!"));
