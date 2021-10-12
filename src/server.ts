2ce26087c826111ab1911b92f3f9b86a0f129d13
lucas couto <tesye30@hotmail.com>
2021-10-12 17:42:38 -0300

    criando especificacao

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { response } from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationRoutes } from './routes/specification.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories",categoriesRoutes);
+app.use("/specifications", specificationRoutes);
 app.listen(8080, () => console.log("Server is running!"));
