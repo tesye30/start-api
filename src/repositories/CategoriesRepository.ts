e3dd690b914f2d84ffd2363288d7711dd83bcc67
lucas couto <tesye30@hotmail.com>
2021-10-11 23:57:33 -0300

    principio lsp categories

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,12 +1,10 @@
 import { response } from "express";
 import { Category } from "../model/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
 
-interface ICreateCategoryDTO {
-    name: string;
-    description: string;
-}
 
-class CategoriesRepository {
+
+class CategoriesRepository implements ICategoriesRepository {
 
     private  categories: Category[];
 
