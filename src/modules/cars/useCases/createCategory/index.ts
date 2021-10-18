3cb9a936d79429afc48201eb584799bf4e935b64
lucas couto <tesye30@hotmail.com>
2021-10-18 00:16:14 -0300

    create use case 1.1

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -10,4 +10,4 @@ const createCategoryController = new CreateCategoryController(
     createCategoryUseCase
 );
 
-export {CreateCategoryController};
+export { createCategoryController };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,15 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { createCategoryController } from '../modules/cars/useCases/createCategory/'
+
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-   const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-   createCategoryService.execute({ name, description});
-
-    return response.status(201).send();
+    return createCategoryController;
 });
 
 categoriesRoutes.get("/", (request, response) => {
